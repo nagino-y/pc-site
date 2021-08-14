fa3c632172554d2e3349942c5aaebdaadca8fc5b
nagino-y <naginoyukichi@gmail.com>
2021-08-14 19:22:07 +0900

    app-reserve

--- sites/first-my-app/src/app/app.module.ts
+++ sites/first-my-app/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './common/navbar/navbar.component';//最後のtをのぞいて自動生成
+import { NavbarComponent } from './common/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
